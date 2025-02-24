671c8b5c91064b5aefae1e70fde2d2cdf6aebef1
famosus <146581744+famosus@users.noreply.github.com>
2025-02-24 10:42:50 +0200

    Update env.ts

--- env.ts
+++ env.ts
@@ -2,5 +2,5 @@ export const EnvVars = {
   SITE_NAME: 'My SaaS Startup',
   OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
   URL: 'https://next-saas-starter-ashy.vercel.app/',
-  MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
+  MAILCHIMP_SUBSCRIBE_URL: 'https://notionarray.us13.list-manage.com/subscribe?u=46c303c5a36484b2595c362aa&id=36b7c5db2b',
 };
